209a8221e717c0e501e775ff58cddd7690424f79
Darron Froese <darron@froese.org>
2014-04-03 12:41:43 -0600

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+name             'sysdig'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures sysdig'
+version          '0.1.0'
+recipe           'sysdig::default', 'Installs/configures sysdig'
+
+depends 'chef-sugar'
